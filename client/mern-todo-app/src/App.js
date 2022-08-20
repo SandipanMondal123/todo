6e864a697f3377d37f7ae714b04219a936f5c40e
Sandipan Mondal <office.smondal@gmail.com>
2022-08-20 15:04:58 -0400

    installed cors and attempting to link frontend and backend

--- client/mern-todo-app/src/App.js
+++ client/mern-todo-app/src/App.js
@@ -1,11 +1,34 @@
 import './App.css';
-
+import axios from 'axios'
+import {useState} from 'react'
 function App() {
+
+  const [itemText, setItemText] = useState('')
+  const addItem = async (e) => {
+    e.preventDefault();
+    try{
+      const res = await axios.post('http://localhost:5500/api/item', {item: itemText});
+      console.log(res)
+    }catch(err)
+    {
+      console.log(err);
+    }
+  }
+
+
+
+
+
+
+
+
+
+
   return (
     <div className="App">
      <h1> Sandipan Mondal's MERN ToDo List App! </h1>
-     <form className = "form">
-      <input type = "text" placeHolder = "Add your ToDo item here!"/>
+     <form className = "form" onSubmit = {e => addItem(e)}>
+      <input type = "text" placeholder = "Add your ToDo item here!" onChange = {e => {setItemText(e.target.value)} } value = {itemText} />
       <button type = "submit"> Add </button>
      </form  >
      <div className = 'todo-listItems'>
